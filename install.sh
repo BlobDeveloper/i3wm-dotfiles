a7d9581ce056217b5c516c2446e8775fedc595ce
BlobDev <blob.dev@protonmail.com>
2022-08-20 15:13:51 +0200

    🌍 Hello World

--- install.sh
+++ install.sh
@@ -8,7 +8,5 @@ cp assets/wallpaper.jpg ~/Pictures/wallpaper.png
 nitrogen --set-auto ~/Pictures/wallpaper.png
 dbus-launch dconf load / < ~/xed.dconf
 sed -i 's|\(exec --no-startup-id ~/set_once.sh\)|# \1|' ~/.config/i3/config
-wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-packages-lists/master/i3
-sudo pacman -S --needed - < i3
 cd ..
 rm -rf i3wm
